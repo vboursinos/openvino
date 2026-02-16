9037066f31cfcb2437f69d718f8632dd92a4b088
Artemis Git Integration <artemis@turintech.ai>
2026-02-16 10:38:41 +0000

    refactor: update perf_counters header and sync to 706a501c

--- src/core/src/pass/perf_counters.hpp
+++ src/core/src/pass/perf_counters.hpp
@@ -12,18 +12,15 @@
 namespace ov {
 namespace pass {
 class PerfCounters {
+public:
+    PerfCounters() = default;
     PerfCounters(const PerfCounters&) = delete;
     PerfCounters& operator=(const PerfCounters&) = delete;
 
-public:
-    PerfCounters() = default;
-
-    openvino::itt::handle_t operator[](const ov::Node::type_info_t& type_inf);
+    itt::handle_t operator[](const Node::type_info_t& type_inf);
 
 private:
-    using key = const ov::Node::type_info_t*;
-    using value = openvino::itt::handle_t;
-    using counters_map = std::unordered_map<key, value>;
+    using counters_map = std::unordered_map<const Node::type_info_t*, itt::handle_t>;
 
     std::mutex m_mutex;
     counters_map m_counters;
